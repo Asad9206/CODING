bd09b45cf5400eacc84eeca155229d8ab41aaabf
Md Asad Anwer <mdasadanwer2005@gmail.com>
2026-08-30 23:37:00 +0530

    Time: 106 ms (43.51%), Space: 249.1 MB (6.87%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,45 +1,37 @@
-import java.util.*;
-
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         int n = nums.length;
-        
-        int[][] elements = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            elements[i][0] = nums[i];
-            elements[i][1] = i;
+
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, List<Integer>> group = new HashMap<>();
+        Map<Integer, Integer> groupId = new HashMap<>();
+        Map<Integer, Integer> pos = new HashMap<>();
+
+        int id = 1;
+        group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[0]);
+        groupId.put(sorted[0], id);
+
+        for(int i = 1; i < n; i++){
+            if(sorted[i] - sorted[i - 1] > limit){
+                id++;
+            }
+
+            group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[i]);
+            groupId.put(sorted[i], id);
         }
-        
-        Arrays.sort(elements, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int[] answer = new int[n];
-        
-        int start = 0;
-        
-        while (start < n) {
-            int end = start;
-            
-            while (end + 1 < n &&
-                   (long) elements[end + 1][0] - elements[end][0] <= limit) {
-                end++;
-            }
-            
-            int[] indices = new int[end - start + 1];
-            
-            for (int i = start; i <= end; i++) {
-                indices[i - start] = elements[i][1];
-            }
-            
-            Arrays.sort(indices);
-            
-            for (int i = 0; i < indices.length; i++) {
-                answer[indices[i]] = elements[start + i][0];
-            }
-            
-            start = end + 1;
+
+        // Rebuild nums using the smallest
+        // available value from its group
+        for(int i = 0; i < n; i++){
+            int grp = groupId.get(nums[i]);
+            int p = pos.getOrDefault(grp, 0);
+
+            nums[i] = group.get(grp).get(p);
+            pos.put(grp, p + 1);
         }
-        
-        return answer;
+
+        return nums;
     }
 }
